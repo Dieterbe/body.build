6092ec8dbcd998e6f5ce403313e8e969a6a3f11a
Dieter Plaetinck <dieter@plaetinck.be>
2024-09-27 16:26:57 +0300

    disclaimer

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -17,10 +17,11 @@ You can look up:
 * joint articulations & involved muscles.
 
 What I would really like to do is make this more visual.  
-Such that you can see the entire ROM (range of motion) of a movement and see how different muscles activate,  
+Such that you can see the entire ROM (range of motion) of a movement and see how different muscles/heads/fibers activate,  
 become stronger at specific points (or have a better moment arm), when they are fully stretched, and deactivate
 
-Have a look at the **shoulder flexion** articulation to get a preview
+The best example of my current progress would be the **shoulder flexion**.  
+To see it, hit the "articulations" button, and then click on "shoulder flexion".
 
 ### DISCLAIMER:
 * use any information here at your own risk
