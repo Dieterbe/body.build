d1d5fa8a4f0074d38c34bfaceb1db882f12aaabb
Dieter Plaetinck <dieter@plaetinck.be>
2025-01-05 13:18:34 +0100

    misc autogen

--- lib/data/programmer/setup.g.dart
+++ lib/data/programmer/setup.g.dart
@@ -6,23 +6,6 @@ part of 'setup.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$currentProfileHash() => r'1c81d80fda6c060feee42ddeb871e1e25d8dec9c';
-
-/// See also [currentProfile].
-@ProviderFor(currentProfile)
-final currentProfileProvider = AutoDisposeFutureProvider<Settings?>.internal(
-  currentProfile,
-  name: r'currentProfileProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$currentProfileHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef CurrentProfileRef = AutoDisposeFutureProviderRef<Settings?>;
 String _$setupHash() => r'a4961ebf11e387238cb411e82e729e86e67d4df3';
 
 /// See also [Setup].
