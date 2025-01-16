00ec74b04f9937553d1a136afc201f3844ceba03
Dieter Plaetinck <dieter@plaetinck.be>
2025-01-16 17:47:31 +0100

    better training EE ui

--- lib/ui/programmer/widget/kv_row.dart
+++ lib/ui/programmer/widget/kv_row.dart
@@ -6,26 +6,35 @@ class KVRow extends StatelessWidget {
   final Widget k;
   final Widget v;
   final String? help;
-  final String? helpTitle; // if help non null, helpTitle is required
-  const KVRow(this.k, this.v, {this.help, this.helpTitle, super.key});
+  final Widget? helpWidget;
+  final String? helpTitle; // required if help or helpWidget != null
+  const KVRow(this.k, this.v,
+      {this.help, this.helpTitle, this.helpWidget, super.key});
 
   @override
   Widget build(BuildContext context) {
     final spacer = MediaQuery.sizeOf(context).width / 150;
+    Widget? hw;
+    if (helpWidget != null) {
+      hw = InfoButton(
+        title: helpTitle!,
+        child: helpWidget!,
+      );
+    } else if (help != null) {
+      hw = InfoButton(
+        title: helpTitle!,
+        child: MarkdownBody(
+          data: help!,
+        ),
+      );
+    }
     return Row(
       children: [
         k,
         SizedBox(width: spacer),
         Expanded(flex: 8, child: v),
-        if (help != null) SizedBox(width: spacer),
-        (help == null)
-            ? SizedBox(width: 40 + spacer)
-            : InfoButton(
-                title: helpTitle!,
-                child: MarkdownBody(
-                  data: help!,
-                ),
-              ),
+        if (hw != null) SizedBox(width: spacer),
+        (hw == null) ? SizedBox(width: 40 + spacer) : hw,
       ],
     );
   }
