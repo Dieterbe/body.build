6ac28712b529edea39a0180af95377edfb609d5b
Dieter Plaetinck <dieter@plaetinck.be>
2025-06-04 19:34:41 +0300

    cleanups

--- lib/ui/programmer/widget/builder_sets.dart
+++ lib/ui/programmer/widget/builder_sets.dart
@@ -35,15 +35,6 @@ class _BuilderSetsState extends ConsumerState<BuilderSets> {
   bool isExpanded = false;
 
   @override
-  void dispose() {
-    // Make sure the notifier is updated when disposed
-    if (isExpanded) {
-      widget.isExpandedNotifier.value = false;
-    }
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final setRatings = widget.sets.getApplicableRatings().toList();
     return Column(
@@ -183,8 +174,6 @@ class _BuilderSetsState extends ConsumerState<BuilderSets> {
       onPressed: () {
         setState(() {
           isExpanded = !isExpanded;
-
-          // Update the expansion notifier
           widget.isExpandedNotifier.value = isExpanded;
         });
       },
