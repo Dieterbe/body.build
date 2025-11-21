74da80b979d446b5b4b0e93a795dde581b8ce18d
Dieter Plaetinck <dieter@plaetinck.be>
2025-11-21 08:08:30 +0200

    cleanup a bit demo1 exercise getting

--- lib/data/programmer/demo_workouts.dart
+++ lib/data/programmer/demo_workouts.dart
@@ -15,6 +15,7 @@ import 'package:bodybuild/model/programmer/workout.dart';
     could be expanded in the future when we introduce e.g. enums for exercise ID's
 */
 const demo1ID = 'demo1';
+Ex _ex(String id) => exes.firstWhere((e) => e.id == id);
 final demo1 = ProgramState(
   name: 'Demo Program 1 - Full body',
   builtin: true,
@@ -23,26 +24,21 @@ final demo1 = ProgramState(
       name: 'day A',
       setGroups: [
         SetGroup([
-          Sets(2, n: 2, ex: exes.firstWhere((e) => e.id == 'romanian deadlift')),
-          Sets(
-            2,
-            n: 2,
-            ex: exes.firstWhere((e) => e.id == 'seated leg extension machine'),
-            tweakOptions: {'lean': 'back'},
-          ),
-          Sets(2, n: 2, ex: exes.firstWhere((e) => e.id == 'chest supported machine row')),
-          Sets(2, n: 2, ex: exes.firstWhere((e) => e.id == 'chest press machine')),
+          Sets(2, n: 2, ex: _ex('romanian deadlift')),
+          Sets(2, n: 2, ex: _ex('seated leg extension machine'), tweakOptions: {'lean': 'back'}),
+          Sets(2, n: 2, ex: _ex('chest supported machine row')),
+          Sets(2, n: 2, ex: _ex('chest press machine')),
         ]),
         SetGroup([
-          Sets(2, n: 2, ex: exes.firstWhere((e) => e.id == 'dumbbell overhead press')),
+          Sets(2, n: 2, ex: _ex('dumbbell overhead press')),
           Sets(
             2,
             n: 2,
-            ex: exes.firstWhere((e) => e.id == 'dumbbell standing calf raise'),
+            ex: _ex('dumbbell standing calf raise'),
             tweakOptions: {'ROM': 'full with lengthened partials beyond failure'},
           ),
-          Sets(1, ex: exes.firstWhere((e) => e.id == 'dumbbell overhead tricep extension')),
-          Sets(1, ex: exes.firstWhere((e) => e.id == 'lying dumbbell bicep curl')),
+          Sets(1, ex: _ex('dumbbell overhead tricep extension')),
+          Sets(1, ex: _ex('lying dumbbell bicep curl')),
         ]),
       ],
       timesPerPeriod: 3,
@@ -55,39 +51,24 @@ final demo1 = ProgramState(
           Sets(
             2,
             n: 2,
-            ex: exes.firstWhere((e) => e.id == 'barbell squat'),
+            ex: _ex('barbell squat'),
             tweakOptions: {'bar': 'front', 'lower leg movement': 'back and forth'},
           ),
-          Sets(
-            2,
-            n: 2,
-            ex: exes.firstWhere((e) => e.id == 'seated cable row'),
-            tweakOptions: {'spine': 'dynamic'},
-          ),
-          Sets(
-            2,
-            n: 2,
-            ex: exes.firstWhere((e) => e.id == 'barbell bench press'),
-            tweakOptions: {'bench angle': '15'},
-          ),
+          Sets(2, n: 2, ex: _ex('seated cable row'), tweakOptions: {'spine': 'dynamic'}),
+          Sets(2, n: 2, ex: _ex('barbell bench press'), tweakOptions: {'bench angle': '15'}),
         ]),
         SetGroup([
           Sets(
             2,
             n: 2,
-            ex: exes.firstWhere((e) => e.id == 'seated leg curl machine'),
+            ex: _ex('seated leg curl machine'),
             tweakOptions: {'ankle dorsiflexed': 'yes'},
           ),
-          Sets(2, n: 2, ex: exes.firstWhere((e) => e.id == 'standing cable lateral raise')),
-          Sets(
-            2,
-            n: 2,
-            ex: exes.firstWhere((e) => e.id == 'standing cable bicep curl'),
-            tweakOptions: {'style': 'bayesian'},
-          ),
-          Sets(2, n: 2, ex: exes.firstWhere((e) => e.id == 'cable overhead tricep extension')),
-          Sets(1, ex: exes.firstWhere((e) => e.id == 'dumbbell wrist flexion')),
-          Sets(1, ex: exes.firstWhere((e) => e.id == 'dumbbell wrist extension')),
+          Sets(2, n: 2, ex: _ex('standing cable lateral raise')),
+          Sets(2, n: 2, ex: _ex('standing cable bicep curl'), tweakOptions: {'style': 'bayesian'}),
+          Sets(2, n: 2, ex: _ex('cable overhead tricep extension')),
+          Sets(1, ex: _ex('dumbbell wrist flexion')),
+          Sets(1, ex: _ex('dumbbell wrist extension')),
         ]),
       ],
       timesPerPeriod: 3,
