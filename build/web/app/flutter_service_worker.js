eccd320c9405b813e7421380006221724b6bc1c1
Dieter Plaetinck <dieter@plaetinck.be>
2025-11-26 12:55:06 +0200

    build

--- build/web/app/flutter_service_worker.js
+++ build/web/app/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "44ccc512e6e4d26e60a1a660a880fd7d",
-"flutter_bootstrap.js": "76b11e10bffdeb44a76d4e8bd3a544c7",
+"flutter_bootstrap.js": "72f5ec2bd181727c08884b63cfff2937",
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "canvaskit/canvaskit.js": "140ccb7d34d0a55065fbd422b843add6",
 "canvaskit/skwasm_heavy.js.symbols": "3c01ec03b5de6d62c34e17014d1decd3",
@@ -22,7 +22,7 @@ const RESOURCES = {"manifest.json": "44ccc512e6e4d26e60a1a660a880fd7d",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "index.html": "1433b0835934602ab5060281fd6d7274",
 "/": "1433b0835934602ab5060281fd6d7274",
-"main.dart.js": "9111de9f7b7df17dfc877cbae533b884",
+"main.dart.js": "bf6d62e3bc661d012cfe5c4b1e7dca9a",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "version.json": "8b3cf47cbae7bc4305a1e260bd94a9e6",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
